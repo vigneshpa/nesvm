90c9f721404a62815cfe1e924708cc935a0f4138
Vignesh Paranthaman <vignesh.pg@zohocorp.com>
2024-11-14 19:18:57 +0530

    adding typecheck, hmr full reload and ignoring non rust files in cargo watch

--- wasmnes/vite.config.ts
+++ wasmnes/vite.config.ts
@@ -1,4 +1,4 @@
-import { ConfigEnv, defineConfig, type PluginOption } from 'vite';
+import { build, ConfigEnv, defineConfig, type PluginOption } from 'vite';
 import { execSync, spawn } from 'node:child_process';
 import wasm from "vite-plugin-wasm";
 
@@ -9,9 +9,20 @@ export default defineConfig((env) => ({
     },
     plugins: [
         wasm(),
+        typescriptCheck(env),
         wasmPackBuildPlugin(env),
     ]
 }))
+
+function typescriptCheck(env: ConfigEnv): PluginOption {
+    return {
+        name: "typescript-check",
+        buildStart() {
+            if (env.mode == "production")
+                execSync('tsc', { stdio: "inherit" })
+        }
+    }
+}
 
 function wasmPackBuildPlugin(env: ConfigEnv): PluginOption {
     let mode = "";
@@ -37,7 +48,7 @@ function wasmPackBuildPlugin(env: ConfigEnv): PluginOption {
             const ignoreList = ['js', 'json', 'ts', 'html', 'css', 'gitignore'];
             const ignoreArgs: string[] = [];
             for (const type of ignoreList) {
-                ignoreArgs .push('-i', '*.' + type);
+                ignoreArgs.push('-i', '*.' + type);
             }
 
             const cargoWatch = spawn('cargo', ['watch', ...ignoreArgs, '-s', wasmPackCmd], {
@@ -58,6 +69,10 @@ function wasmPackBuildPlugin(env: ConfigEnv): PluginOption {
             server.httpServer?.on('close', () => {
                 cargoWatch.kill();
             });
-        }
+        },
+        handleHotUpdate({ server }) {
+            server.ws.send({ type: "full-reload" });
+            return [];
+          }
     }
 }
